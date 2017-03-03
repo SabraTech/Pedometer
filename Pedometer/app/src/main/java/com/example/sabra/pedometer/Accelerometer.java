eeae5862692c3a0cde7183ce0e0406e8b122ee86
sabra <mohamed2006fs@gmail.com>
2017-03-03 17:35:15 +0200

    Change the icon image and reformat the code

--- Pedometer/app/src/main/java/com/example/sabra/pedometer/Accelerometer.java
+++ Pedometer/app/src/main/java/com/example/sabra/pedometer/Accelerometer.java
@@ -5,22 +5,14 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.net.Uri;
 import android.os.SystemClock;
-import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -32,21 +24,15 @@ public class Accelerometer extends AppCompatActivity implements SensorEventListe
     private SensorManager sensorManager;
     private Sensor accelerometer;
 
-    private double deltaX = 0;
-    private double deltaY = 0;
-    private double deltaZ = 0;
-    private double lastX = 0, lastY = 0 , lastZ = 0;
-    private int steps = 0;
-    private int counter = 0;
+    private double deltaX = 0, deltaY = 0, deltaZ = 0;
+    private double lastX = 0, lastY = 0, lastZ = 0;
+    private int steps = 0, counter = 0, oldSteps = 0;
+    ;
     private double height = 0.0;
 
-    private long startTime = 0;
-    private int oldSteps = 0;
+    private long startTime = 0, startWatch = 0, stopWatch = 0;
     private int walked = 0;
-    private long startWatch = 0;
-    private long stopWatch = 0;
-    private double meters = 0;
-    private double runningSpeed = 0;
+    private double meters = 0, runningSpeed = 0;
 
     private ArrayList<Double> x;
     private ArrayList<Double> y;
@@ -56,28 +42,28 @@ public class Accelerometer extends AppCompatActivity implements SensorEventListe
     private ArrayList<Double> dataY;
     private ArrayList<Double> dataZ;
 
-    private double vibrateThreshold = 0.0;
-
     private TextView stepsNumber, distance, speed, calories;
     private Chronometer watch;
     private Button start, stop;
     private EditText heightData;
 
-    public Vibrator v;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accelerometer);
+
         initializeViews();
+
         startTime = System.currentTimeMillis();
+
         x = new ArrayList<Double>();
         y = new ArrayList<Double>();
         z = new ArrayList<Double>();
+
         dataX = new ArrayList<Double>();
         dataY = new ArrayList<Double>();
         dataZ = new ArrayList<Double>();
+
         start.setOnClickListener(this);
         stop.setOnClickListener(this);
 
@@ -89,17 +75,17 @@ public class Accelerometer extends AppCompatActivity implements SensorEventListe
         start.setEnabled(true);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        if(sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null){
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
             // success
             accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
-        }else{
+        } else {
             // error
         }
 
     }
 
-    public void initializeViews(){
+    public void initializeViews() {
         stepsNumber = (TextView) findViewById(R.id.stepsNumber);
         distance = (TextView) findViewById(R.id.distance);
         speed = (TextView) findViewById(R.id.speed);
@@ -113,7 +99,7 @@ public class Accelerometer extends AppCompatActivity implements SensorEventListe
     @Override
     public void onClick(View v) {
 
-        if(v.equals(start)){
+        if (v.equals(start)) {
 
             watch.setBase(SystemClock.elapsedRealtime() + stopWatch);
             watch.start();
@@ -133,7 +119,8 @@ public class Accelerometer extends AppCompatActivity implements SensorEventListe
 
             sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 
-        }else if(v.equals(stop)){
+        } else if (v.equals(stop)) {
+
             stepsNumber.setEnabled(false);
             distance.setEnabled(false);
             speed.setEnabled(false);
@@ -141,17 +128,17 @@ public class Accelerometer extends AppCompatActivity implements SensorEventListe
             stop.setEnabled(false);
             start.setEnabled(true);
             heightData.setEnabled(true);
+
             stopWatch = watch.getBase() - SystemClock.elapsedRealtime();
             watch.stop();
+
             sensorManager.unregisterListener(this);
 
-
         }
-
     }
 
+    public void resetValues() {
 
-    public void  resetValues(){
         watch.setBase(SystemClock.elapsedRealtime());
         meters = 0;
         steps = 0;
@@ -163,12 +150,12 @@ public class Accelerometer extends AppCompatActivity implements SensorEventListe
         calories.setText("0");
     }
 
-    protected  void onResume(){
+    protected void onResume() {
         super.onResume();
         sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
-    protected  void onPause(){
+    protected void onPause() {
         super.onPause();
         sensorManager.unregisterListener(this);
     }
@@ -182,29 +169,29 @@ public class Accelerometer extends AppCompatActivity implements SensorEventListe
 
 
         // if the change is below 2, it is just noise
-        if(deltaX > precision ){
+        if (deltaX > precision) {
             x.add((double) event.values[0]);
             lastX = event.values[0];
-        }else{
+        } else {
             x.add(lastX);
         }
-        if(deltaY > precision){
+        if (deltaY > precision) {
             y.add((double) event.values[1]);
             lastY = event.values[1];
-        }else{
+        } else {
             y.add(lastY);
         }
-        if(deltaZ > precision){
+        if (deltaZ > precision) {
             z.add((double) event.values[2]);
             lastZ = event.values[2];
-        }else{
+        } else {
             z.add(lastZ);
         }
 
         counter++;
 
-        if(counter == 200){
-            if(System.currentTimeMillis() > startTime + 200){
+        if (counter == 200) {
+            if (System.currentTimeMillis() > startTime + 200) {
                 walked = steps - oldSteps;
                 startTime = System.currentTimeMillis();
                 oldSteps = steps;
@@ -216,46 +203,47 @@ public class Accelerometer extends AppCompatActivity implements SensorEventListe
 
         }
 
-        //vibrate();
     }
 
-    public void calculateData(){
+    public void calculateData() {
 
         // distance
-        if(walked > 0 && walked < 2){
-            meters += height/5.0;
-        }else if(walked >= 2 && walked < 3){
-            meters += height/4.0;
-        }else if(walked >= 3 && walked < 4){
-            meters += height/3.0;
-        }else if(walked >= 4 && walked < 5){
-            meters += height/2.0;
-        }else if(walked >= 5 && walked < 6){
-            meters += height/1.2;
-        }else if(walked >= 6 && walked < 8){
+        if (walked > 0 && walked < 2) {
+            meters += height / 5.0;
+        } else if (walked >= 2 && walked < 3) {
+            meters += height / 4.0;
+        } else if (walked >= 3 && walked < 4) {
+            meters += height / 3.0;
+        } else if (walked >= 4 && walked < 5) {
+            meters += height / 2.0;
+        } else if (walked >= 5 && walked < 6) {
+            meters += height / 1.2;
+        } else if (walked >= 6 && walked < 8) {
             meters += height;
-        }else if(walked >= 8){
-            meters += height*1.2;
+        } else if (walked >= 8) {
+            meters += height * 1.2;
         }
 
-        // speed
-        runningSpeed = meters/(((System.currentTimeMillis() - startWatch) * 1000));
+        // update speed
+        runningSpeed = meters / (((System.currentTimeMillis() - startWatch) * 1000));
         speed.setText(Double.toString(runningSpeed));
 
-        calories.setText(Double.toString((runningSpeed*3.6) * 1.25));
+        // update calories
+        calories.setText(Double.toString((runningSpeed * 3.6) * 1.25));
 
+        // update distance
         distance.setText(Double.toString(meters));
     }
 
-    public void updateSteps(){
+    public void updateSteps() {
 
         // Filter Out the data
-        for(int i = 0; i < 200; i += 4) {
+        for (int i = 0; i < 200; i += 4) {
             double sumX = 0, sumY = 0, sumZ = 0;
-            for(int j = 0; j < 4 && i + j < 200; j++){
-                sumX += x.get(i+j);
-                sumY += y.get(i+j);
-                sumZ += z.get(i+j);
+            for (int j = 0; j < 4 && i + j < 200; j++) {
+                sumX += x.get(i + j);
+                sumY += y.get(i + j);
+                sumZ += z.get(i + j);
             }
             dataX.add(sumX / 4);
             dataY.add(sumY / 4);
@@ -270,48 +258,54 @@ public class Accelerometer extends AppCompatActivity implements SensorEventListe
 
         // select the axis to work on
         int workingAxis = -1; // 0 = x, 1 = y, 2 = z
-        double diffX = Collections.max(dataX) - Collections.min(dataX);
-        double diffY = Collections.max(dataY) - Collections.min(dataY);
-        double diffZ = Collections.max(dataZ) - Collections.min(dataZ);
-        double maxDiff = Math.max(diffX, Math.max(diffY,diffZ));
 
-        if(maxDiff == diffX){
+        double maxX = Collections.max(dataX), minX = Collections.min(dataX);
+        double maxY = Collections.max(dataY), minY = Collections.min(dataY);
+        double maxZ = Collections.max(dataZ), minZ = Collections.min(dataZ);
+
+        double diffX = maxX - minX ;
+        double diffY = maxY - minY;
+        double diffZ = maxZ - minZ;
+
+        double maxDiff = Math.max(diffX, Math.max(diffY, diffZ));
+
+        if (maxDiff == diffX) {
             workingAxis = 0;
-        }else if(maxDiff == diffY){
+        } else if (maxDiff == diffY) {
             workingAxis = 1;
-        }else{
+        } else if (maxDiff == diffZ) {
             workingAxis = 2;
         }
 
         // check how many steps now
-        if(workingAxis == 0){
-            double average = (Collections.max(dataX) + Collections.min(dataX)) / 2;
-            for(int i = 0, j = 1; i < 49; i++, j++){
-                if(average > dataX.get(j) && average < dataX.get(i)){
+        if (workingAxis == 0) {
+            double average = (maxX + minX) / 2;
+            for (int i = 0, j = 1; i < 49; i++, j++) {
+                if (average > dataX.get(j) && average < dataX.get(i)) {
                     steps++;
                     displayCurrentsValues();
                 }
             }
 
-        }else if(workingAxis == 1){
-            double average = (Collections.max(dataY) + Collections.min(dataY)) / 2;
-            for(int i = 0, j = 1; i < 49; i++, j++){
-                if( average > dataY.get(j) && average < dataY.get(i)){
+        } else if (workingAxis == 1) {
+            double average = (maxY + minY) / 2;
+            for (int i = 0, j = 1; i < 49; i++, j++) {
+                if (average > dataY.get(j) && average < dataY.get(i)) {
                     steps++;
                     displayCurrentsValues();
                 }
             }
 
-        }else if(workingAxis == 2){
-            double average = (Collections.max(dataZ) + Collections.min(dataZ)) / 2;
-            for(int i = 0, j = 1; i < 49; i++, j++){
-                if( average > dataZ.get(j) && average < dataZ.get(i)){
+        } else if (workingAxis == 2) {
+            double average = (maxZ + minZ) / 2;
+            for (int i = 0, j = 1; i < 49; i++, j++) {
+                if (average > dataZ.get(j) && average < dataZ.get(i)) {
                     steps++;
                     displayCurrentsValues();
                 }
             }
 
-        }else{
+        } else {
             // error
         }
 
@@ -324,7 +318,7 @@ public class Accelerometer extends AppCompatActivity implements SensorEventListe
     }
 
 
-    public void displayCurrentsValues(){
+    public void displayCurrentsValues() {
         stepsNumber.setText(Integer.toString(steps));
     }
 
@@ -332,5 +326,4 @@ public class Accelerometer extends AppCompatActivity implements SensorEventListe
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
-
 }
